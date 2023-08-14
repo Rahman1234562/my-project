19966611a00ca546bae296cf7f343ae03e8e41fd
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-08-14 04:25:39 -0700

    updated files

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>about</div>
+    <div>this is about page</div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,14 +17,25 @@ export default function Home() {
       </Head>
       <nav className="main-nav">
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Blogs</li>
+          <Link href="/">  <li>Home</li> </Link>
+          <Link href="/about">  <li>About</li> </Link>
+          <Link href="/contact">  <li>Contact</li> </Link>
+          <Link href="/blogs">  <li>Blogs</li> </Link>
         </ul>
       </nav>
       <div className="main-para">
-        <h1>problem solve coder</h1>
+        <h1 className="code dummy">problem solve coder</h1>
+        <Image className="home-img" src="/homeimg.avif" alt="this is coder image" width={250} height={160}/>
+        <style jsx>
+          {`
+          h2{
+            font-size: 38px;
+          }
+          h3{
+            font-size: 28px;
+          }
+          `}
+        </style>
         <p className="para">all the programming language solution get <span className="here">here</span></p>
         <h2 className="popular">Popular Blogs</h2>
         <h3 className="javascript-para">How to learn javaScript in 2023?</h3>

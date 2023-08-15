59497df5d525d4a0642d0db5d7df5a97c9190957
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-08-15 12:18:21 -0700

    updated files

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import classes from "@/styles/blogs.module.css";
-import Link from 'next/link';
-
+import Link from "next/link";
 
 const Blogs = () => {
+  const [Blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    console.log("useEffect is running");
+    fetch("http://localhost:3000/api/blog")
+      .then((a) => {
+        return a.json();
+      })
+      .then((parsed) => {
+        console.log(parsed)
+        setBlogs(parsed);
+      });
+  }, []);
   return (
-    <div className={classes.Blogs}>
-        <h2 className={classes.popular}>Popular Blogs</h2>
-        <Link href={"/blogPost/learn-javaScript"}> <h3 className={classes.main2}>How to learn javaScript in 2023?</h3></Link>
+    <div className={classes.main4}>
+      {Blogs.map((blogitem) => {
+        return<div key={blogitem.title}>
+        <Link href={"/blogPost/learn-javaScript"}>
+          <h3 className={classes.main2}>{blogitem.title}</h3></Link>
         <p>javaScript is a language used for web logic</p>
+      </div>
+      })}
+      
+      <div>
         <h3 className={classes.main2}>How to learn javaScript in 2023?</h3>
         <p>javaScript is a language used for web logic</p>
+      </div>
+      <div>
         <h3 className={classes.main2}>How to learn javaScript in 2023?</h3>
         <p>javaScript is a language used for web logic</p>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Blogs;

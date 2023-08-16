d9ad6e31670531b3b015621fbcd21cb61873029f
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-08-16 12:29:26 -0700

    updated the files

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -2,18 +2,30 @@
 import * as fs from 'fs';
 
 export default async function handler(req, res) {
-  let data = await fs.promises.readdir("blogData")
-  let myFile;
-  let allBlogs = [];
-  for (let index = 0; index < data.length; index++) {
-    const item = data[index];
-    console.log(item)
-    myFile = await fs.promises.readFile(("blogData/" + item), 'utf-8')
-   allBlogs.push(JSON.parse(myFile))
+  try {
+    let data = await fs.promises.readdir("blogData");
+    let allBlogs = [];
 
-    
+    for (let index = 0; index < data.length; index++) {
+      const item = data[index];
+      console.log(item);
+
+      try {
+        let myFile = await fs.promises.readFile(('blogData/' + item), 'utf-8');
+        let parsedContent = JSON.parse(myFile);
+        allBlogs.push(parsedContent);
+      } catch (error) {
+        console.error(`Error parsing file "${item}": ${error}`);
+      }
+    }
+
+    const formattedJSON = JSON.stringify(allBlogs, null, 2); // Use space parameter for indentation
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).end(formattedJSON);
+
+  } catch (error) {
+    console.error(`Error reading directory: ${error}`);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-
-      res.status(200).json(allBlogs);
-
 }
+
--- pages/blogs.js
+++ pages/blogs.js
@@ -3,36 +3,28 @@ import classes from "@/styles/blogs.module.css";
 import Link from "next/link";
 
 const Blogs = () => {
-  const [Blogs, setBlogs] = useState([]);
+  const [Blog, setBlog] = useState([]);
   useEffect(() => {
     console.log("useEffect is running");
-    fetch("http://localhost:3000/api/blog")
-      .then((a) => {
+    fetch('http://localhost:3000/api/blog').then((a) => {
         return a.json();
       })
       .then((parsed) => {
         console.log(parsed)
-        setBlogs(parsed);
+        setBlog(parsed);
       });
-  }, []);
+  },[]);
   return (
     <div className={classes.main4}>
-      {Blogs.map((blogitem) => {
-        return<div key={blogitem.title}>
-        <Link href={"/blogPost/learn-javaScript"}>
+      {Blog.map((blogitem) => {
+        return(
+         <div key={blogitem.slug}>
+       <Link href={`/blogPost/${blogitem.slug}`}>
           <h3 className={classes.main2}>{blogitem.title}</h3></Link>
-        <p>javaScript is a language used for web logic</p>
+        <p className={classes.mainpara}>{blogitem.content.substr(0, 140)}... </p>
       </div>
+      )
       })}
-      
-      <div>
-        <h3 className={classes.main2}>How to learn javaScript in 2023?</h3>
-        <p>javaScript is a language used for web logic</p>
-      </div>
-      <div>
-        <h3 className={classes.main2}>How to learn javaScript in 2023?</h3>
-        <p>javaScript is a language used for web logic</p>
-      </div>
     </div>
   );
 };

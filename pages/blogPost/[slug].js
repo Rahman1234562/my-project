f39dbd1f08c916a56067e6cb8a4894b72fbff6a9
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-08-17 09:59:30 -0700

    updated files

--- pages/blogPost/[slug].js
+++ pages/blogPost/[slug].js
@@ -1,18 +1,31 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from '../../styles/BlogPost.module.css'
 
 
 const Slug = () => {
+  const [blog, setBlog] = useState();
     const router = useRouter();
-    const {slug} = router.query;
+        
+    useEffect(() => {
+      if(!router.isReady) return;
+      const {slug} = router.query;
+      fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+        .then((a) => {
+          return a.json();
+        })
+        .then((parsed) => {
+          setBlog(parsed);
+        });
+    }, [router.isReady]);
+
    
   return (
     <div className={classes.blogsPost}>
-        <h1>Title of the page {slug}</h1>
-        <div className={classes.para}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur dolorum consequuntur dolore dolor optio dignissimos ducimus in delectus laboriosam voluptatem perferendis nemo, minima omnis necessitatibus exercitationem impedit cum natus rem atque repudiandae modi sequi earum! Quos explicabo itaque ullam vel, inventore odio dicta aperiam, adipisci veritatis modi sunt provident officiis rem quaerat pariatur? Exercitationem et ad incidunt reiciendis sunt velit aspernatur dolores aliquid ullam nostrum, iure assumenda, similique debitis illo delectus quam vero recusandae ratione, quidem maiores optio deserunt cumque. Minima aspernatur accusantium nesciunt repudiandae deleniti corporis officia, eaque molestias excepturi, ea dignissimos perferendis. Dignissimos dolore aspernatur quia odio amet.</div>
+        <h1>{blog && blog.title}</h1>
+        <div className={classes.para}>{blog && blog.content}</div>
     </div>
   )
 }
 
-export default Slug
+export default Slug;
--- pages/blogs.js
+++ pages/blogs.js
@@ -3,27 +3,29 @@ import classes from "@/styles/blogs.module.css";
 import Link from "next/link";
 
 const Blogs = () => {
-  const [Blog, setBlog] = useState([]);
+  const [blogs, setBlogs] = useState([]);
   useEffect(() => {
-    console.log("useEffect is running");
-    fetch('http://localhost:3000/api/blog').then((a) => {
+    fetch("http://localhost:3000/api/blog")
+      .then((a) => {
         return a.json();
       })
       .then((parsed) => {
-        console.log(parsed)
-        setBlog(parsed);
+        setBlogs(parsed);
       });
-  },[]);
+  }, []);
   return (
     <div className={classes.main4}>
-      {Blog.map((blogitem) => {
-        return(
-         <div key={blogitem.slug}>
-       <Link href={`/blogPost/${blogitem.slug}`}>
-          <h3 className={classes.main2}>{blogitem.title}</h3></Link>
-        <p className={classes.mainpara}>{blogitem.content.substr(0, 140)}... </p>
-      </div>
-      )
+      {blogs.map((blogitem) => {
+        return (
+          <div key={blogitem.slug}>
+            <Link href={`/blogPost/${blogitem.slug}`}>
+              <h3 className={classes.main2}>{blogitem.title}</h3>
+            </Link>
+            <p className={classes.mainpara}>
+              {blogitem.content.substr(0, 140)}...{" "}
+            </p>
+          </div>
+        );
       })}
     </div>
   );
